3c44ef6cfbbde22504e67320fbc58d09c6b8506d
kento.okamoto <mntymnty6@gmail.com>
2022-11-08 02:18:47 +0000

    kadai 18-1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.create(content: 'test content 1')
+Task.create(content: 'test content 2')
+Task.create(content: 'test content 3')
