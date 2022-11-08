8c9fabb60a6b613319987b043d2cd72620f5633e
kento.okamoto <mntymnty6@gmail.com>
2022-11-08 07:40:21 +0000

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_07_170641) do
+ActiveRecord::Schema.define(version: 2022_11_08_024944) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
+    t.string "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
