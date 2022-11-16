6bcf02413b184ccc505dd1426efea91a5d1c01a5
kento.okamoto <mntymnty6@gmail.com>
2022-11-16 15:13:22 +0000

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
+  
+  # protectedじゃなくて良いんだ
+  private
+
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
 end
